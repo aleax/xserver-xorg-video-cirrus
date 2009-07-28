4ecaa548a996475fbb4c6403698b2ecaf35e9d6b
Dave Airlie <airlied@redhat.com>
2009-07-28 15:22:40 +1000

    cirrus: change to using ABI version check

--- src/alp_driver.c
+++ src/alp_driver.c
@@ -36,7 +36,7 @@
 /* This driver needs to be modified to not use vgaHW for multihead operation */
 #include "vgaHW.h"
 
-#ifndef XSERVER_LIBPCIACCESS
+#if GET_ABI_MAJOR(ABI_VIDEODRV_VERSION) < 6
 #include "xf86RAC.h"
 #include "xf86Resources.h"
 #endif
